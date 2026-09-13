4aee21d1c4f5988f95a29ebb6cb96f745a211645
rendeb-dsb <87785469+rendeb-dsb@users.noreply.github.com>
2026-09-13 10:43:17 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="count-v12";
+const CACHE="count-v13";
 const FILES=["./","./index.html","./manifest.json","./sw.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
